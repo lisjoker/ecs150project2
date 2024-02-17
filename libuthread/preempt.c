a7b6013185ac5182b882884c2aac2c1bf761f466
lisjoker <91924172+lisjoker@users.noreply.github.com>
2024-02-16 21:47:48 -0800

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -13,89 +13,61 @@
  * Frequency of preemption
  * 100Hz is 100 times per second
  */
-
 #define HZ 100
-#define microseconds 1000000
+static struct sigaction old_action;
+static struct itimerval timer;
+static bool preempt_enabled = false;
 
-// global variable so that preempt_start and preempt_stop can access it
-struct sigaction new_action, old_action;
-struct itimerval old_timer, new_timer;
-
-void handler(int signum) {
-	if (signum == SIGVTALRM) {
-		uthread_yield();
-	}
-
-	return;
+static void preempt_signal_handler(int signum) {
+    if (signum == SIGVTALRM) {
+        uthread_yield(); // Force the currently running thread to yield
+    }
 }
 
-
 void preempt_disable(void) {
-	sigset_t block_alarm;
-	sigemptyset(&block_alarm);
-	sigaddset(&block_alarm, SIGVTALRM);
-	sigprocmask(SIG_BLOCK, &block_alarm, NULL);
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGVTALRM);
+    sigprocmask(SIG_BLOCK, &set, NULL); // Block SIGVTALRM signals
+    preempt_enabled = false;
 }
 
 void preempt_enable(void) {
-	sigset_t unblock_alarm;
-	sigemptyset(&unblock_alarm);
-	sigaddset(&unblock_alarm, SIGVTALRM);
-	sigprocmask(SIG_UNBLOCK, &unblock_alarm, NULL);
-}
-/*
- * preempt_start - Start thread preemption
- *
- * Configure a timer that must fire a virtual alarm at a frequency of 100 Hz and
- * setup a timer handler that forcefully yields the currently running thread.
- */
-void preempt_start(bool) {
-	// Idea:
-	// Every 100 Hz (by alarm), write a handler that calls uthread_yield
-
-	new_action.sa_handler = handler;
-	sigemptyset(&new_action.sa_mask);
-	new_action.sa_flags = 0;
-
-	if (sigaction(SIGVTALRM, &new_action, &old_action) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-
-	// We want to have a timer interrupt every 100Hz
-	// 100 Hz = 0.01 seconds (T = 1/f)
-	// "usec" uses microseconds
-	// 0.01 seconds = 10,000 microseconds
-	// There are 1000000 microseconds in a second
-	// to get 0.01 seconds, divide 1000000/100 (microseconds / HZ)
-
-	new_timer.it_interval.tv_usec = microseconds / HZ;
-  	new_timer.it_interval.tv_sec = 0; // Don't have whole number of seconds
-  	new_timer.it_value.tv_usec = microseconds / HZ;
-  	new_timer.it_value.tv_sec = 0; // Don't have whole number of seconds
-
-	if (setitimer(ITIMER_VIRTUAL, &new_timer, &old_timer) == -1) {
-		perror("setitimer");
-		exit(1);
-	}
-
-    return;
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGVTALRM);
+    sigprocmask(SIG_UNBLOCK, &set, NULL); // Unblock SIGVTALRM signals
+    preempt_enabled = true;
 }
 
-/*
- * preempt_stop - Stop thread preemption
- *
- * Restore previous timer configuration, and previous action associated to
- * virtual alarm signals.
- */
+void preempt_start(bool preempt) {
+    if (preempt) {
+        struct sigaction action;
+        struct itimerval new_timer;
+
+        // Set up the signal handler
+        action.sa_handler = preempt_signal_handler;
+        sigemptyset(&action.sa_mask);
+        action.sa_flags = 0;
+        sigaction(SIGVTALRM, &action, &old_action);
+
+        // Configure the timer
+        new_timer.it_interval.tv_sec = 0;
+        new_timer.it_interval.tv_usec = 1000000 / HZ;
+        new_timer.it_value = new_timer.it_interval;
+        setitimer(ITIMER_VIRTUAL, &new_timer, &timer);
+    }
+}
+
 void preempt_stop(void) {
-	if (sigaction(SIGVTALRM, &old_action, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
+    if (preempt_enabled) {
+        // Restore the old signal handler
+        sigaction(SIGVTALRM, &old_action, NULL);
 
-	if (setitimer(ITIMER_VIRTUAL, &old_timer, NULL) == -1) {
-		perror("setitimer");
-		exit(1);
-	}
+        // Disable the timer
+        timer.it_interval.tv_sec = 0;
+        timer.it_interval.tv_usec = 0;
+        timer.it_value = timer.it_interval;
+        setitimer(ITIMER_VIRTUAL, &timer, NULL);
+    }
 }
