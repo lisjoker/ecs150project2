fc76a82647da369cdf38630996250de40623bf17
lisjoker <91924172+lisjoker@users.noreply.github.com>
2024-02-16 21:09:37 -0800

    Update test_preempt.c

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -2,39 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <unistd.h>
 #include <uthread.h>
 #include <preempt.c>
 #include <private.h>
 
-void thread3(void *arg)
+
+static void thread5(void *arg)
 {
 	(void)arg;
-	printf("\nI'm in thread 3, exit.\n");
+	printf("it's thread5,eixt\n");
 	exit(0);
+} 
+static void thread4(void *arg)
+{
+	(void)arg;
+	uthread_create(thread5, NULL);
+	while (1) {
+		
+		printf("thread4 is working\n");
+		uthread_yield(); 
+        
+    }
+} 
+static void thread3(void *arg)
+{
+	(void)arg;
+	uthread_create(thread4, NULL);
+	while (1) {
+		
+		printf("thread3 is working\n");
+		sleep(1);
+        
+    }
 }
-
-void thread2(void *arg)
+static void thread2(void *arg)
 {
 	(void)arg;
 	uthread_create(thread3, NULL);
-	while (1)
-	{
-		printf("2");
-	}
+	while (1) {
+		sleep(1);
+		printf("thread2 is working\n");
+        
+    }
 }
 
-void thread1(void *arg)
+static void thread1(void *arg)
 {
 	(void)arg;
 	uthread_create(thread2, NULL);
-	while (1)
-	{
-		printf("1");
-	}
+	while (1) {
+		sleep(1);
+		printf("thread1 is working\n");
+        
+    }
 }
 
 int main(void)
 {
-	uthread_run(true, thread1, NULL);
+	uthread_run(true,thread1, NULL);
 	return 0;
 }
