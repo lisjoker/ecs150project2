50f7d6593aa15486b024963532ddb09322448ba5
Shijie Wei <112788068+usernameDelet@users.noreply.github.com>
2024-02-16 23:29:48 -0800

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -1,15 +1,12 @@
 #include <stddef.h>
 #include <stdlib.h>
-
 #include "queue.h"
 #include "sem.h"
 #include "private.h"
 #include "uthread.h"
-
 // Define return error value.
 #define ERR -1
 #define SUCC 0
-
 struct semaphore 
 {
 	/* TODO Phase 3 */
@@ -17,7 +14,6 @@ struct semaphore
     queue_t waitQueue;
     uthread_ctx_t* blockedCtx;  // To keep track of the blocked thread's context
 };
-
 sem_t sem_create(size_t count)
 {
 	/* TODO Phase 3 */
@@ -27,37 +23,30 @@ sem_t sem_create(size_t count)
 		// Fail to create allocate heap
         return NULL;
     }
-
 	// Initialize the variable of the new semaphore
     newSemaphore->count = count;
     newSemaphore->blockedCtx = NULL;
     newSemaphore->waitQueue = queue_create();
-
     return newSemaphore;
 }
-
 int sem_destroy(sem_t sem)
 {
 	/* TODO Phase 3 */
 	int vaild;
-
-	if ((sem == NULL) || (queue_length(sem->waitQueue) == 0)) 
+	if ((sem == NULL) || (queue_length(sem->waitQueue) == 0))
 	{
-        return ERR; // Invalid semaphore or threads still blocked
+		// Invalid semaphore or threads still blocked
+        return ERR;
     }
-
     vaild = queue_destroy(sem->waitQueue);
-
 	if(vaild == ERR)
 	{
 		// There are still threads in queue
 		return ERR;
 	}
-
     free(sem);
-
-    return SUCC; // Successful destruction
-
+	// Successful destruction
+    return SUCC;
 }
 
 int sem_down(sem_t sem) 
@@ -67,7 +56,6 @@ int sem_down(sem_t sem)
 	{
         return ERR; // Invalid semaphore
     }
-
 	while (sem->count == 0)
 	{
 		if (sem->count > 0) 
@@ -76,28 +64,27 @@ int sem_down(sem_t sem)
 			sem->count--;
 			// Unblock the previously blocked thread
 			uthread_unblock(newThread);
-		} 
+		}
 		else 
 		{
-			uthread_block();  // Block the current thread
+			// Block the current thread
+			uthread_block();
 		}
 	}
 	sem->count--;
-
-    return SUCC; // Successful down operation
+	// Successful down operation
+    return SUCC;
 }
-
 int sem_up(sem_t sem) 
 {
-    if (sem == NULL) 
+    if (sem == NULL)
 	{
         return ERR; // Invalid semaphore
     }
-
 	int vaild;
     sem->count++;
 	// waiting list associated to @sem is not empty
-    if (queue_length(sem->waitQueue) == 0) 
+    if (queue_length(sem->waitQueue) == 0)
 	{
         // Unblock the first thread from the wait_queue
         struct uthread_tcb *blocked_thread;
@@ -110,6 +97,6 @@ int sem_up(sem_t sem)
 		sem->blockedCtx = NULL;
         uthread_unblock(blocked_thread);
     }
-
-    return SUCC; // Successful up operation
+	// Successful up operation
+    return SUCC;
 }
